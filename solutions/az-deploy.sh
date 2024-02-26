079e0b883cfbc13b707554108e1376fd2f688446
Julia Kordick <moin@jkordick.dev>
2024-02-26 14:27:22 +0100

    Fix deployment script

--- solutions/az-deploy.sh
+++ solutions/az-deploy.sh
@@ -1,18 +1,18 @@
 # use the azure cli to deploy the app to an azure app service
 # env variables (use azure naming convention)
-$RESOURCE_GROUP = "rg-stu-offsite-ghcopilot"
-$APP_SERVICE_PLAN = "asp-stu-offsite-ghcopilot"
-$APP_SERVICE = "app-stu-offsite-ghcopilot"
-$LOCATION = "westeurope"
+RESOURCE_GROUP="rg-stu-offsite-ghcopilot"
+APP_SERVICE_PLAN="asp-stu-offsite-ghcopilot"
+APP_SERVICE="app-stu-offsite-ghcopilot"
+LOCATION="westeurope"
 
 # create a resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
 # create an app service plan
-az appservice plan create --name $APP_SERVICE_PLAN --resource-group $RESOURCE_GROUP --location $LOCATION --sku FREE --is-linux
+az appservice plan create --name $APP_SERVICE_PLAN --resource-group $RESOURCE_GROUP --location $LOCATION --sku B1 --is-linux
 
 # create an app service
 az webapp create --name $APP_SERVICE --resource-group $RESOURCE_GROUP --plan $APP_SERVICE_PLAN --runtime "NODE:18-lts"
 
 # deploy the app
-az webapp deployment --name $APP_SERVICE --resource-group $RESOURCE_GROUP
+az webapp up --name $APP_SERVICE --resource-group $RESOURCE_GROUP
